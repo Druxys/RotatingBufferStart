28ff33db3d4e0ba11d1841cbd5451bb63e3debe8
Paul Turpin <turpinpaulpro@gmail.com>
2021-12-16 09:34:37 +0100

    project done

--- src/com/jad/RotatingBuffer/RotatingBufferReader.java
+++ src/com/jad/RotatingBuffer/RotatingBufferReader.java
@@ -1,13 +1,23 @@
 package com.jad.RotatingBuffer;
 
 class RotatingBufferReader<Data> extends RotatingBufferActor<Data> implements IRotatingBufferReader<Data>{
-    @Override
-    public Data read() {
-        return null;
+
+    public RotatingBufferReader(RotatingBuffer<Data> buffer) {
+        super(buffer);
     }
 
     @Override
-    public int getIndex() {
-        return 0;
+    public Data read() {
+        RotatingBuffer<Data> buffer = getBuffer();
+        RotatingBufferWriter<Data> writer = buffer.getWriter();
+        if ((this.getIndex() != writer.getIndex()) || !buffer.isEmpty()) {
+            Data[] dataArray = buffer.getDataArray();
+            Data temp = dataArray[this.getIndex()];
+            incrementIndex();
+            if (this.getIndex() == writer.getIndex())
+                buffer.setEmpty(true);
+            return temp;
+        }
+        return null;
     }
 }
